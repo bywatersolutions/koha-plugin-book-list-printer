ddf2be33017eb93182f9820e1517a2274d4b7012
Kyle M Hall <kyle@bywatersolutions.com>
2023-02-23 18:22:47 +0000

    Add ititial MVP

--- Koha/Plugin/Com/ByWaterSolutions/BookListPrinter.pm
+++ Koha/Plugin/Com/ByWaterSolutions/BookListPrinter.pm
@@ -6,15 +6,12 @@ use base qw(Koha::Plugins::Base);
 
 use C4::Auth;
 use C4::Context;
+use Koha::Items;
 
 use Cwd qw(abs_path);
 use Data::Dumper;
-use LWP::UserAgent;
-use MARC::Record;
-use Mojo::JSON qw(decode_json);;
-use URI::Escape qw(uri_unescape);
 
-our $VERSION = "{VERSION}";
+our $VERSION         = "{VERSION}";
 our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
 
 our $metadata = {
@@ -29,7 +26,7 @@ our $metadata = {
 };
 
 sub new {
-    my ( $class, $args ) = @_;
+    my ($class, $args) = @_;
 
     ## We need to add our metadata here so our base class can access it
     $args->{'metadata'} = $metadata;
@@ -43,143 +40,44 @@ sub new {
     return $self;
 }
 
-## The existance of a 'report' subroutine means the plugin is capable
-## of running a report. This example report can output a list of patrons
-## either as HTML or as a CSV file. Technically, you could put all your code
-## in the report method, but that would be a really poor way to write code
-## for all but the simplest reports
 sub report {
-    my ( $self, $args ) = @_;
+    my ($self, $args) = @_;
     my $cgi = $self->{'cgi'};
 
-    unless ( $cgi->param('output') ) {
+    unless ($cgi->param('output')) {
         $self->report_step1();
-    }
-    else {
+    } else {
         $self->report_step2();
     }
 }
 
-## If your tool is complicated enough to needs it's own setting/configuration
-## you will want to add a 'configure' method to your plugin like so.
-## Here I am throwing all the logic into the 'configure' method, but it could
-## be split up like the 'report' method is.
-sub configure {
-    my ( $self, $args ) = @_;
-    my $cgi = $self->{'cgi'};
-
-    unless ( $cgi->param('save') ) {
-        my $template = $self->get_template({ file => 'configure.tt' });
-
-        ## Grab the values we already have for our settings, if any exist
-        $template->param(
-            enable_opac_payments => $self->retrieve_data('enable_opac_payments'),
-            foo             => $self->retrieve_data('foo'),
-            bar             => $self->retrieve_data('bar'),
-            last_upgraded   => $self->retrieve_data('last_upgraded'),
-        );
-
-        $self->output_html( $template->output() );
-    }
-    else {
-        $self->store_data(
-            {
-                enable_opac_payments => $cgi->param('enable_opac_payments'),
-                foo                => $cgi->param('foo'),
-                bar                => $cgi->param('bar'),
-                last_configured_by => C4::Context->userenv->{'number'},
-            }
-        );
-        $self->go_home();
-    }
-}
-
 sub report_step1 {
-    my ( $self, $args ) = @_;
+    my ($self, $args) = @_;
     my $cgi = $self->{'cgi'};
 
-    my $template = $self->get_template({ file => 'report-step1.tt' });
+    my $template = $self->get_template({file => 'report-step1.tt'});
 
-    my @libraries = Koha::Libraries->search;
-    my @categories = Koha::Patron::Categories->search({}, {order_by => ['description']});
-    $template->param(
-        libraries => \@libraries,
-        categories => \@categories,
-    );
-
-    $self->output_html( $template->output() );
+    $self->output_html($template->output());
 }
 
 sub report_step2 {
-    my ( $self, $args ) = @_;
+    my ($self, $args) = @_;
     my $cgi = $self->{'cgi'};
 
-    my $dbh = C4::Context->dbh;
+    my $filename = 'report-step2-html.tt';
+    my $template = $self->get_template({file => $filename});
 
-    my $branch                = $cgi->param('branch');
-    my $category_code         = $cgi->param('categorycode');
-    my $borrower_municipality = $cgi->param('borrower_municipality');
-    my $output                = $cgi->param('output');
+    my $order_by
+        = $cgi->param('displayby') eq 'title'   ? 'biblio.title'
+        : $cgi->param('displayby') eq 'author'  ? 'biblio.author'
+        : $cgi->param('displayby') eq 'subject' ? 'subject'
+        :                                         'title';
 
-    my $fromDay   = $cgi->param('fromDay');
-    my $fromMonth = $cgi->param('fromMonth');
-    my $fromYear  = $cgi->param('fromYear');
+    my $items = Koha::Items->search({}, {prefetch => 'biblio', order_by => {-asc => $order_by}});
 
-    my $toDay   = $cgi->param('toDay');
-    my $toMonth = $cgi->param('toMonth');
-    my $toYear  = $cgi->param('toYear');
+    $template->param(items => $items);
 
-    my ( $fromDate, $toDate );
-    if ( $fromDay && $fromMonth && $fromYear && $toDay && $toMonth && $toYear )
-    {
-        $fromDate = "$fromYear-$fromMonth-$fromDay";
-        $toDate   = "$toYear-$toMonth-$toDay";
-    }
-
-    my $query = "
-        SELECT firstname, surname, address, city, zipcode, city, zipcode, dateexpiry FROM borrowers 
-        WHERE branchcode LIKE '$branch'
-        AND categorycode LIKE '$category_code'
-    ";
-
-    if ( $fromDate && $toDate ) {
-        $query .= "
-            AND DATE( dateexpiry ) >= DATE( '$fromDate' )
-            AND DATE( dateexpiry ) <= DATE( '$toDate' )  
-        ";
-    }
-
-    my $sth = $dbh->prepare($query);
-    $sth->execute();
-
-    my @results;
-    while ( my $row = $sth->fetchrow_hashref() ) {
-        push( @results, $row );
-    }
-
-    my $filename;
-    if ( $output eq "csv" ) {
-        print $cgi->header( -attachment => 'borrowers.csv' );
-        $filename = 'report-step2-csv.tt';
-    }
-    else {
-        print $cgi->header();
-        $filename = 'report-step2-html.tt';
-    }
-
-    my $template = $self->get_template({ file => $filename });
-
-    $template->param(
-        date_ran     => dt_from_string(),
-        results_loop => \@results,
-        branch       => GetBranchName($branch),
-    );
-
-    unless ( $category_code eq '%' ) {
-        $template->param( category_code => $category_code );
-    }
-
-    print $template->output();
+    $self->output_html($template->output());
 }
 
 
